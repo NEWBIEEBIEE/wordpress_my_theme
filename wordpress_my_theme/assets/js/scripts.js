f247beaf46562029365e5f6e8b4351919af76b3d
NEWBIEEBIEE <myconstantcollegelife@gmail.com>
2022-09-04 20:48:41 +0900

    add file

--- wordpress_my_theme/assets/js/scripts.js
+++ wordpress_my_theme/assets/js/scripts.js
@@ -1,6 +1,9 @@
 $(function() {
-    const hum = $('#hamburger, .close')
-    const nav = $('.sp-nav')
+
+    const nav = $('.sp-nav');
+    const closeButton = nav.find('.menu').children('ul');
+    closeButton.append('<li class="close"><span>閉じる</span></li>');
+    const hum = $('#hamburger, .close');
     hum.on('click', function(){
        nav.toggleClass('toggle');
     });
