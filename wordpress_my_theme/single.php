924eb5ef752e9da50ceec5ddb686f5ab89fbdfa4
NEWBIEEBIEE <myconstantcollegelife@gmail.com>
2023-03-25 10:44:33 +0900

    add and edit

--- wordpress_my_theme/single.php
+++ wordpress_my_theme/single.php
@@ -10,7 +10,8 @@
         <h2 class="serif single_title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?> </a></h2>
         <div class="time-data">
             <p>
-                <span class="category_name"><?php echo $cat_name; ?></span><?php echo get_the_date('Y年n月j日'); ?>
+                <div class="cat-name"><?php echo $cat_name; ?></div>
+                <span><?php echo get_the_date('Y年n月j日'); ?></span>
             </p>
         </div>
         <div class="contents_area">
@@ -21,15 +22,3 @@
         <?php get_sidebar(); ?>
     </div>
 <?php get_footer(); ?>
-<style>
-article h3 {
-    border-bottom: 2px solid #6ab547;
-    margin-bottom: 20px;
-    font-size: 28px;
-    font-weight: 600;
-    background-image: url(../images/h2_icon.png);
-    background-repeat: no-repeat;
-    background-position: left bottom;
-    padding: 20px 10px 10px 48px;
-}
-</style>
